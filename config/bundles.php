558c6294bc787787e5d410d9c502d28773dacc89
EstelleMyddleware <estellegaits@myddleware.com>
2022-06-30 15:19:25 +0200

    b RefreshToken Entity PHPDoc triggered Doctrine error - swapped to PHP8 attributes instead

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
