cececb71b87d47bec7cb14c0de9355ad58a5e049
EstelleMyddleware <estellegaits@myddleware.com>
2022-07-11 10:10:45 +0200

    tests: Product entity - test POST requests not allowed & GET requests with no auth return 401

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -3,14 +3,29 @@
 namespace App\Tests;
 
 use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class ProductTest extends ApiTestCase
 {
-    public function testSomething(): void
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testReadProductsWithNoJWTToken(): void
     {
-        $response = static::createClient()->request('GET', '/');
+        $response = static::createClient()->request('GET', '/api/products');
+        $this->assertResponseStatusCodeSame(401);
+    }
 
-        $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/']);
+    /**
+     * @throws TransportExceptionInterface
+     */
+    public function testCreateProductIsNotAllowed(): void
+    {
+        $response = static::createClient()->request('POST', '/api/products',[
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => [],
+        ]);
+        // POST route is forbidden on this API
+        $this->assertResponseStatusCodeSame(405);
     }
 }
